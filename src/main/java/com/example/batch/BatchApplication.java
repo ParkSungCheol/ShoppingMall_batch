d885ba5a490bee566a42003dafe59aeb7e1bb4ac
ParkSungCheol <skfo8gmlakd@naver.com>
2023-05-02 17:53:58 +0900

    test

--- src/main/java/com/example/batch/BatchApplication.java
+++ src/main/java/com/example/batch/BatchApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BatchApplication {
 
 	public static void main(String[] args) {
+		System.out.println("test");
 		SpringApplication.run(BatchApplication.class, args);
 	}
 
