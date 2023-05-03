b5866bf605aebaaaa31346a5fb94e4b7684a4a76
ParkSungCheol <skfo8gmlakd@naver.com>
2023-05-03 18:04:49 +0900

    동적 크롤링 테스트중..

--- src/main/java/com/example/batch/tasklet/TestTasklet.java
+++ src/main/java/com/example/batch/tasklet/TestTasklet.java
@@ -1,8 +1,18 @@
 package com.example.batch.tasklet;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.StepContribution;
@@ -10,6 +20,8 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.stereotype.Component;
+
+import io.netty.handler.timeout.TimeoutException;
 
 @Component
 public class TestTasklet implements Tasklet {
@@ -19,10 +31,83 @@ public class TestTasklet implements Tasklet {
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
         String url = "https://search.shopping.naver.com/search/all?query=해피머니";
-        Document doc = Jsoup.connect(url).get();
-        
-        Elements elem = doc.select("div.basicList_title__VfX3c");
-        log.info(elem.toString());
+        runSelenium(url, log);
         return RepeatStatus.FINISHED;
+    }
+    
+    private static void runSelenium(String URL, Logger log) throws Exception {
+    	log.info("#### START ####");
+        
+        // 1. WebDriver 경로 설정
+        Path path = Paths.get("C:\\ShoppingMall/batch/driver/geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", path.toString());
+        
+        // 2. WebDriver 옵션 설정
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--start-maximized");          // 최대크기로
+        options.addArguments("--headless");                 // Browser를 띄우지 않음
+        options.addArguments("--disable-gpu");              // GPU를 사용하지 않음, Linux에서 headless를 사용하는 경우 필요함.
+        options.addArguments("--no-sandbox");               // Sandbox 프로세스를 사용하지 않음, Linux에서 headless를 사용하는 경우 필요함.
+        options.addArguments("--disable-popup-blocking");    // 팝업 무시
+        options.addArguments("--disable-default-apps");     // 기본앱 사용안함
+        
+        // 3. WebDriver 객체 생성
+        WebDriver driver = new FirefoxDriver( options );
+                
+        try {
+            
+            // 4. 웹페이지 요청
+            driver.get(URL);
+            
+            // 5. 페이지 로딩을 위한 최대 5초 대기
+            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+            
+            // 6. 조회, 로드될 때까지 최대 10초 대기
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            String byFunKey = "CSSSELECTOR";
+            String selectString = "div.list_basis div div:nth-child(6) div div div:nth-child(2)";
+//            String byFunKey = "XPATH";
+//            String selectString = "//*[@id=\"mArticle\"]/div[2]/ul/li[3]/a";
+            WebElement parent = wait.until(ExpectedConditions.presenceOfElementLocated( 
+                    byFunKey.equals("XPATH") ? By.xpath(selectString) : By.cssSelector(selectString) ));
+//            log.info("#### innerHTML : \n" + parent.getAttribute("innerHTML"));
+            
+            // 7. 콘텐츠 조회
+            List<WebElement> bestContests = parent.findElements(By.cssSelector("div.section_best > ul > li > a"));
+            log.info( "best 콘텐츠 수 : " + bestContests.size() );
+            if (bestContests.size() > 0) {
+                for (WebElement best : bestContests) {
+                    String title = best.findElement(By.cssSelector("div.wrap_cont > strong > span")).getText();
+                    String name = best.findElement(By.cssSelector("div.info_g > span.txt_id")).getText();
+                    log.info("Best title / blog name : " + title + " / " + name);
+                    log.info("Best url : " + best.getAttribute("href"));
+                }
+            }
+            
+            log.info("########################################");
+            
+            List<WebElement> contents = parent.findElements(By.cssSelector("div.section_list > ul > li > a"));
+            log.info( "조회된 콘텐츠 수 : " + contents.size() );
+            if( contents.size() > 0 ) {
+                for(WebElement content : contents ) {
+                    String title = content.findElement(By.cssSelector("div.wrap_cont > strong > span")).getText();
+                    String name = content.findElement(By.cssSelector("div.info_g > span.txt_id")).getText();
+                    log.info("콘텐츠 title / blog name : " + title + " / " + name);
+                    log.info("콘텐츠 url : " + content.getAttribute("href"));
+                }
+            }
+            
+        } catch ( TimeoutException e ) {
+            e.printStackTrace();
+            log.info(e.toString());            
+        } catch ( Exception e ) {
+            e.printStackTrace();
+            log.info(e.toString());         
+        }
+        
+        // 8. WebDriver 종료
+        driver.quit();
+        
+        log.info("#### END ####");
     }
 }
