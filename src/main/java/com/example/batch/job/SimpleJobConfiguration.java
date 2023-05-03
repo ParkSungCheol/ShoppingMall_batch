7a19e778968a862b34a4bcd49a063cbd1419e8ad
ParkSungCheol <skfo8gmlakd@naver.com>
2023-05-03 16:37:12 +0900

    test

--- src/main/java/com/example/batch/job/SimpleJobConfiguration.java
+++ src/main/java/com/example/batch/job/SimpleJobConfiguration.java
@@ -1,21 +1,17 @@
 package com.example.batch.job;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.example.batch.UniqueRunIdIncrementer;
 import com.example.batch.tasklet.TestTasklet;
+
+import lombok.RequiredArgsConstructor;
 
 /*
 --job.name=incrementerJob
