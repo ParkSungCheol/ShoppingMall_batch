b1242d1118c65dfeb4eea1f1180ae7ca10a75f36
ParkSungCheol <skfo8gmlakd@naver.com>
2023-05-16 15:43:19 +0900

    TEST

--- src/main/java/com/example/batch/config/JobCompletionNotificationListener.java
+++ src/main/java/com/example/batch/config/JobCompletionNotificationListener.java
@@ -2,14 +2,16 @@ package com.example.batch.config;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
+
 import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,16 +20,16 @@ public class JobCompletionNotificationListener implements JobExecutionListener {
     private final DataSource dataSource;
     private Connection connection;
     private Logger log = LoggerFactory.getLogger(this.getClass());
-    private final Map<Long, BatchStatus> jobStatusMap = new HashMap<>();
-
-    public JobCompletionNotificationListener(DataSource dataSource) {
+    private TaskExecutor taskExecutor;
+    
+    public JobCompletionNotificationListener(DataSource dataSource, TaskExecutor taskExecutor) {
         this.dataSource = dataSource;
+        this.taskExecutor = taskExecutor;
     }
 
     @Override
     public void beforeJob(JobExecution jobExecution) {
     	try {
-    		jobStatusMap.put(jobExecution.getJobId(), jobExecution.getStatus());
             if (connection == null || connection.isClosed()) {
                 connection = dataSource.getConnection();
                 log.info("db connection opend");
@@ -39,9 +41,8 @@ public class JobCompletionNotificationListener implements JobExecutionListener {
 
     @Override
     public void afterJob(JobExecution jobExecution) {
-    	jobStatusMap.put(jobExecution.getJobId(), jobExecution.getStatus());
-    	if (isAllJobsCompleted()) {
-    		// 데이터베이스 연결 해제
+    	ThreadPoolTaskExecutor tte = (ThreadPoolTaskExecutor) taskExecutor;
+    	if(tte.getActiveCount() == 0) {
     		try {
                 if (connection != null && !connection.isClosed()) {
                     connection.close();
@@ -50,19 +51,15 @@ public class JobCompletionNotificationListener implements JobExecutionListener {
             } catch (SQLException e) {
                 e.printStackTrace();
             }
-    		// 모든 Job이 완료되었다면 애플리케이션 종료
-//            SpringApplication.exit(jobExecution.getJobInstance().get(), () -> 0);
-        };
-    }
-    
-    private boolean isAllJobsCompleted() {
-    	Boolean flag = true;
-        // 모든 Job의 상태를 확인하여 COMPLETED가 아닌 경우가 있는지 확인
-        for (BatchStatus status : jobStatusMap.values()) {
-            if (status != BatchStatus.COMPLETED) {
-            	flag = false;
-            }
-        }
-        return flag;
+    		tte.shutdown();
+    		
+    		BatchStatus batchStatus = jobExecution.getStatus();
+    		if(batchStatus.isGreaterThan(BatchStatus.STOPPING)) {
+    			System.exit(0);
+    		}
+    		else {
+    			System.exit(1);
+    		}
+    	}
     }
 }
