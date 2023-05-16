4fa8926a6e413cb6db32f3cd93736917cfdd320a
ParkSungCheol <skfo8gmlakd@naver.com>
2023-05-16 14:19:55 +0900

    test

--- src/main/java/com/example/batch/config/BatchConfig.java
+++ src/main/java/com/example/batch/config/BatchConfig.java
@@ -29,8 +29,8 @@ public class BatchConfig implements BatchConfigurer {
     @Bean
     public TaskExecutor taskExecutor() {
         ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-        taskExecutor.setCorePoolSize(3);
-        taskExecutor.setMaxPoolSize(3);
+        taskExecutor.setCorePoolSize(4);
+        taskExecutor.setMaxPoolSize(4);
         taskExecutor.setThreadNamePrefix("batch-thread-");
         taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
         taskExecutor.setAwaitTerminationSeconds(-1);
