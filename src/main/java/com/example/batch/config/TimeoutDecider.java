ce57df284a2c4fa66e8e4bf2a79339c320806b19
ParkSungCheol <skfo8gmlakd@naver.com>
2023-06-08 10:54:07 +0900

    test

--- src/main/java/com/example/batch/config/TimeoutDecider.java
+++ src/main/java/com/example/batch/config/TimeoutDecider.java
@@ -3,17 +3,14 @@ package com.example.batch.config;
 import org.openqa.selenium.TimeoutException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.StepExecution;
-import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.core.job.flow.FlowExecutionStatus;
 import org.springframework.batch.core.job.flow.JobExecutionDecider;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TimeoutDecider implements JobExecutionDecider, StepExecutionListener {
+public class TimeoutDecider implements JobExecutionDecider {
 
     private Logger log = LoggerFactory.getLogger(this.getClass());
     
@@ -28,17 +25,5 @@ public class TimeoutDecider implements JobExecutionDecider, StepExecutionListene
 			log.info("timeoutOccurred not entered");
             return new FlowExecutionStatus("COMPLETED");
         }
-	}
-
-	@Override
-	public void beforeStep(StepExecution stepExecution) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public ExitStatus afterStep(StepExecution stepExecution) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 }
